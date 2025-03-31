fb6d115c62d764d8c631800af47a624a71105021
Dimitar-Totov <dimitar.totov@abv.bg>
2025-03-31 20:20:28 +0300

    Update likesApi.js

--- client/src/api/likesApi.js
+++ client/src/api/likesApi.js
@@ -1,6 +1,8 @@
 import { useEffect } from "react";
+
 import useAuth from "../hooks/useAuth";
 import useStateHook from "../hooks/useStateHook"
+
 import request from "../utils/request";
 
 const baseUrl = 'http://localhost:3030/data/likes'
