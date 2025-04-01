94f99e956be101fa42120d25b81f299b0b34bb38
Dimitar-Totov <dimitar.totov@abv.bg>
2025-04-01 22:40:49 +0300

    Fix style

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -3,7 +3,6 @@ import { Link } from "react-router";
 export default function Footer() {
     return (
         <footer className="bg-gray-200" aria-labelledby="footer-heading">
-            <h2 id="footer-heading" className="sr-only">Footer</h2>
             <div className="mx-auto max-w-7xl px-6 pb-8 pt-16 sm:pt-24 lg:px-8 lg:pt-32">
                 <div className="xl:grid xl:grid-cols-3 xl:gap-8">
                     <div className="space-y-8">
